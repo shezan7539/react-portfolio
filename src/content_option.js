596c3db35803a2d2bbbed1e296c09cd6ddd41b0d
shezan7539 <75947263+shezan7539@users.noreply.github.com>
2024-08-26 17:19:35 +0600

    content_option.js

--- src/content_option.js
+++ src/content_option.js
@@ -9,10 +9,12 @@ const introdata = {
     animated: {
         first: "E-sPorts Player",
         second: "Social Media Expert",
-        third: "Based In Naogaon Sadar",
+        third: "Carder",
+        fourth: "Content Creater",
+        fifth: "Based In Naogaon Sadar",
     },
-    description: "Hey There, This Is Shezan. Welcome To My Portfolio",
-    your_img_url: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/439787759_1590036428456810_4417784619752816003_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeH--f0lDpzouchT9vGXpGKBxYR3r87TrozFhHevztOujHqzBSq4zfb0nV2UiJpBRtgRvy3F0LLvj48Qa4xgZxHB&_nc_ohc=m3oCoIlfoW8Q7kNvgG3CUJ3&_nc_ht=scontent.fdac8-1.fna&oh=00_AYD7dwhzD2jLh-CGXHNIv1hJ71suS0dAtWCfbxCW6TFxkA&oe=66D22DFB",
+    description: "Hey There, This Is MD Mizanur Rahman (Shezan). Welcome To My Portfolio",
+    your_img_url: "https://imgur.com/a/wQBzMvJ",
 };
 
 const dataabout = {
@@ -73,72 +75,72 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/455136778_1660354071425045_8580115927455368769_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF5dN91dMgJgZiHvwbvjFQ-W9bbXRszaZhb1ttdGzNpmLWnbTurTothD598wPLZN9lPofM1g4dGAySWCqYPtQ7X&_nc_ohc=aObaFmakA08Q7kNvgE5KIkB&_nc_ht=scontent.fdac8-1.fna&oh=00_AYBDDvK7Mi9BROZX_Q0CcvTM2HFA32B-O8Bub-j-a1DfpQ&oe=66D22BC3",
+        description: "Picture 01.",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/426392666_1551793248947795_184965181350188671_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeERR-YeRaX6pevmTLGK-gxnSbYDrPp5rvZJtgOs-nmu9h6axiqKAGrzu7-5rnZgXM4ppuR-Us_e5V7noGwHCqXf&_nc_ohc=w3OqiuPhRRkQ7kNvgHteois&_nc_ht=scontent.fdac8-1.fna&oh=00_AYB3QSPJQ5uGxT187SlsncHr0M92RA9k_2hlkFJXE54YNw&oe=66D24242",
+        description: "Picture 02",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/416600396_1527870071340113_83243104998561244_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeE7Ejzs_DfleYRIzi1sXJLP36JrEa6yzOjfomsRrrLM6EhMt3XU48wT_y6bM9nEBtVClJgOvkXFYyyjLFA44v3d&_nc_ohc=7YSg5MlvM8oQ7kNvgFYEUgh&_nc_ht=scontent.fdac8-1.fna&oh=00_AYCe6BHnxxdMXOlSYTPJGqhYwyQ5wVRcm0dhQqoOoiatLg&oe=66D22EF3",
+        description: "Picture 03",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/413820301_1520613008732486_3097117681943840771_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHSKWjrpZ54P3H5DiGssuLjoFXyEbpiEk-gVfIRumIST4dygx7A7pSOgNYSmFKGNULpp0NnQDfvzxvFrX7iL5F6&_nc_ohc=XqQ76jcYK38Q7kNvgEn3lPd&_nc_ht=scontent.fdac8-1.fna&oh=00_AYDpciI4FK0wllF07-h6saKjdvJkyX1NP6P6upPRpjhY2Q&oe=66D24F06",
+        description: "Picture 04",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/400615106_1499519644175156_3592790010239473543_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeE4Nk9b0CQqBDDEb1smBKo9f9KOCliVjol_0o4KWJWOifwXucgoYF8RibbYiWNsBvMehsXeBqSVLh39qfNTaIie&_nc_ohc=WwymGD1D6k0Q7kNvgGguuVp&_nc_ht=scontent.fdac8-1.fna&oh=00_AYAJ6hu9Ge6BvrfuNdm-oOX7BARk7p5rMWHqFWMKSnuMEA&oe=66D23AE9",
+        description: "Picture 05",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/400528188_1499516560842131_5787557095358421900_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeF3SaXqmTRux4nw-R1dKocFC3O93jTsg4QLc73eNOyDhEuvKynTL_BBQgVqTqCKN2fiRyOJFSHOf2qxQUJDpvnN&_nc_ohc=07iHmacS6U4Q7kNvgFoVEDG&_nc_ht=scontent.fdac8-1.fna&oh=00_AYCNM4yymAYW1kP1AhrP72pMOp5ubDXDGW6SkEohGhMEqw&oe=66D21CA5",
+        description: "Picture 06",
         link: "#",
     },
 
     {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/427909171_1552031218923998_8587203063207870565_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeGWZfQPDkbXPe9RlcD7Zt3V-liakcuUEW76WJqRy5QRbmUx-NuqRy8NXvpCada2qEYW90MfLNhqjiMIqJyVXRmb&_nc_ohc=SGWmnGWe5OQQ7kNvgGpdf5Z&_nc_ht=scontent.fdac8-1.fna&oh=00_AYC2meMO-cnCOzb8S7xCNnUSyVf1U85A1XoUgWJKDivftA&oe=66D22D1A",
+        description: "Picture 07",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/431013199_1563255817801538_8709204963502589536_n.jpg?stp=cp6_dst-jpg&_nc_cat=104&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFyzusddo1wvSnWwl3-K4g9nTIjwGxq4JedMiPAbGrgl8iKNuJNxdq1f00E6uOLMDa-iD13Bhp29KBkuzIR3qK6&_nc_ohc=lNAErYJNps0Q7kNvgFIoHLR&_nc_ht=scontent.fdac8-1.fna&oh=00_AYB8Dc9y9ITsIx0HSEn0ne61z-ZTG5GzZtx6Z_I0RqkN5w&oe=66D21FC1",
+        description: "Picture 08",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/436268020_1582507662543020_741593809427257537_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeG7uOk_0S7YyroAdoe-hqHBOcAQUxEZmgc5wBBTERmaBxB47fDR1ZoH3NL95FvunaJ5MtANwwkhGeCjN44xLQE_&_nc_ohc=sqp3WGHh1P0Q7kNvgHZI-F3&_nc_ht=scontent.fdac8-1.fna&oh=00_AYDEhrWoJbDuJwHXDSj3khuaeagXlmxfkvs8ZpzlGuGh9w&oe=66D22796",
+        description: "Picture 09",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/448267670_1621344638659322_3011713640144187467_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeEBL2xkbYM3lom3NLeCPdvk_CM2LRphXJ78IzYtGmFcnhfNKWja_JlCFqpA_kq9NWx_MA0WQm38VSp2Rl-SVpB7&_nc_ohc=u2WF65s35c0Q7kNvgHa_Kpi&_nc_ht=scontent.fdac8-1.fna&_nc_gid=Ap0T9iq8F_yr6Mp4r9RzQda&oh=00_AYCOM-Iigqr49EaL9sQaXLn3BIM17XFgs18NH1ClWkeqUg&oe=66D23844",
+        description: "Picture 10",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/447276864_1614240296036423_4563686667755269079_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGIG6EWZeocYXaSu9p-VIVVDi8iLFTS6KUOLyIsVNLopaFawrpkNYChaV9YdX0zxDYPg-IQPsojLGFVTWk86rnh&_nc_ohc=if2hIKZD3hsQ7kNvgG3sxh3&_nc_ht=scontent.fdac8-1.fna&oh=00_AYDRu8TvZz_ktm5Gnr1QC-BYreyxjfUFcQYd6sZbpQgPJg&oe=66D22C23",
+        description: "Picture 11",
         link: "#",
     },
     {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://scontent.fdac8-1.fna.fbcdn.net/v/t39.30808-6/429808408_1563253311135122_2859276616822765856_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGcKsVtNNi_NHXZ-bHwDYXhlWHzSuDVZK-VYfNK4NVkr2VdQ2nxP4ydmfiXZ9vuepi2fNSW-HIR4_qef_cr2cSz&_nc_ohc=MVi1taZn00AQ7kNvgHnqUdd&_nc_ht=scontent.fdac8-1.fna&oh=00_AYCGikSDs40qTfctKYKN5AgTq_mFC0eCN57tlaTVY7Zhug&oe=66D24FFC",
+        description: "Picture 12",
         link: "#",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "mdmizanurrahmanshezan@duck.com",
+    YOUR_FONE: "+8801741847759",
+    description: "Here's My Contact Information. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
     YOUR_SERVICE_ID: "service_id",
